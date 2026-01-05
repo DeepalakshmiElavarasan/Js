6b7ac4553f6f99132b2363c2e9b2f9a504db6679
Deepalakshmi <146326704+DeepalakshmiElavarasan@users.noreply.github.com>
2026-01-05 13:48:44 +0530

    Add JavaScript data types demonstration
    This file demonstrates various data types in JavaScript, including number, boolean, string, undefined, null, and bigint, along with their type checks.

---
+++ js basics/datatype.js
@@ -0,0 +1,63 @@
+console.log("datatype in Java Script");
+
+
+//1.NUMBER
+
+let num1=30
+console.log("number is "+ num1);
+
+let num2=30.5
+console.log("number is "+ num2);
+
+//typeof
+console.log(typeof num1); //number
+console.log(typeof num2) //number
+
+// (without giving semicolon we can able to run this program not an issue)
+
+
+// 2.BOOLEAN
+let isplayer=true;
+
+console.log(isplayer);
+console.log(typeof isplayer);
+
+// 3.STRING
+                          //1. way to create string
+let str1="good morning java Script"
+console.log(str1);
+console.log(typeof str1);
+                            //2. way to create string
+let str2='good mrng js'
+console.log(str2);
+console.log(typeof str2);
+                          //3. way to create string
+let str3=`gm java` //Backtick
+console.log(str3);
+console.log(typeof str3);
+
+//4.UNDEFINED
+
+let salary;
+console.log(salary)
+console.log(typeof salary)
+
+//5.null  
+
+let id=null;
+console.log(id);
+console.log(typeof id); //!object
+
+
+//bigint
+let num3=2n
+console.log(num3);
+console.log(typeof num3);
+
+
+
+
+
+
+
+
